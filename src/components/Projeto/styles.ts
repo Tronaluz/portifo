3dab9f4a89bf4f8f853936a6cb8161dbeed11b11
tronaluz <lucas_tron@hotmail.com>
2024-11-27 22:40:58 -0300

    Add Projeto component and enhance Projetos container with project listing

---
+++ src/components/Projeto/styles.ts
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const Card = styled.div`
+  boder: 1px solid #C1C1C1;
+  padding: 16px;
+`;
+
+export const LinkBotao = styled.a `
+  color: #fff;
+  cont-size: 14px;
+  background-color: #4476BF;
+  text-decoration: none;
+  padding: 8px ;
+  display: inline-block;
+  margin-top: 24px;
+`
