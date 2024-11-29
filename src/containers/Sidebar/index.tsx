3d2cf3553d355fae25d79dc8b5cd5bfdc8316da5
tronaluz <lucas_tron@hotmail.com>
2024-11-28 21:43:00 -0300

    Add Avatar component, enhance Sidebar with new layout and styling, and update Paragrafo for customizable font size

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import Titulo from '../../components/Titulo';
+import Avatar from '../../components/Avatar';
+import Paragrafo from '../../components/Paragrafo';
+
+import { Descricao, BotaoTema, SidebarContainer } from './styles';
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/Tronaluz.png"/>
-    <Titulo fontSize={20}>Lucas Oliveira</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Lucas Oliveira</Titulo>
+      <Paragrafo tipo="secundario" font-size={16}>Tronaluz</Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Front-End Engineer
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
